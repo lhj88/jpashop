56fbe7d2c7152e9a39412981d0e73cdc508c4b91
lhj88 <dlgmlwn777@naver.com>
2021-02-16 08:26:03 +0900

    jpa 상속

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -20,8 +21,9 @@ public class JpaMain {
 
         try{
 
-            Order order = new Order();
-            order.addOrderItem(new OrderItem());
+            Book book = new Book();
+            book.setName("JPA Test");;
+            book.setAuthor("author");
 
 
             tx.commit();
